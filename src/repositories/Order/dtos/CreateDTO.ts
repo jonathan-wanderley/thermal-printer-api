4ca318a0467f3ac050d52574c1a15ca98bfb941a
Jonathan <jonathan.wpc@gmail.com>
2022-08-23 22:01:15 -0300

    feat: modify properties in CreateOrder DTOs

---
+++ src/repositories/Order/dtos/CreateDTO.ts
@@ -0,0 +1,16 @@
+export default interface CreateOrderDTO {
+  clientName: string;
+  products: [
+    {
+      id: string;
+      name: string;
+    },
+  ];
+  payment: {
+    orderTotal: number;
+    amountPaid: number;
+    moneyChange: number;
+    methods: string;
+  };
+  note: string;
+}
