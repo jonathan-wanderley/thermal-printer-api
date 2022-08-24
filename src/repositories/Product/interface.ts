eba7f5b7e6c08a7a401d18116ddc9e130b67b358
Jonathan <jonathan.wpc@gmail.com>
2022-08-23 22:09:38 -0300

    feat: add countTotalPrice and returnArrayWithName to ProductRepository

--- src/repositories/Product/interface.ts
+++ src/repositories/Product/interface.ts
@@ -1,3 +1,5 @@
 export default interface IProductRepository {
   find(payload: any): Promise<any>;
+  countTotalPrice(productList: any): Promise<any>;
+  returnArrayWithName(IdList: Array<string>): Promise<any>;
 }
