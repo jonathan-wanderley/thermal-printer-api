d0c3d6b643e606d5bd080ba4c60734d74424c444
Jonathan <jonathan.wpc@gmail.com>
2022-08-23 22:10:33 -0300

    feat: add create function to OrderRepository

--- src/repositories/Order/index.ts
+++ src/repositories/Order/index.ts
@@ -2,12 +2,14 @@
 import { Model } from 'mongoose';
 import IOrder from '../../entities/interfaces/IOrder';
 import Order from '../../entities/Order';
+import CreateOrderDTO from './dtos/CreateDTO';
 import IOrderRepository from './interface';
 
 export default class OrderRepository implements IOrderRepository {
-  create(payload: any): Promise<any> {
-    throw new Error('Method not implemented.');
-  }
-
   private orderModel: Model<IOrder> = Order;
+
+  async create(payload: CreateOrderDTO): Promise<any> {
+    const createdUser = await this.orderModel.create(payload);
+    return createdUser;
+  }
 }
