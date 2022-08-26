054fd315be66805a2064badfe3bf72a7c2a78f3d
Jonathan <jonathan.wpc@gmail.com>
2022-08-25 21:02:12 -0300

    feat: implement web socket connection and events

--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -1,9 +1,16 @@
-import { Server as SocketServer } from 'socket.io'
-import { DefaultEventsMap } from 'socket.io/dist/typed-events'
+import { Server as SocketServer } from 'socket.io';
+import { DefaultEventsMap } from 'socket.io/dist/typed-events';
+import socketEvents from './events';
 
 const server = (io: SocketServer<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>) => {
+
   io.on('connection', (socket) => {
-    console.log('conexao detectada');
+    console.log('[WebSocket] Connected sucessfuly!');
+
+    socketEvents.finishOrder(socket);
+    socketEvents.deleteOrder(socket);
+    socketEvents.kitchenDashboard(socket);
+    socketEvents.withdrawlDashboard(socket);
   });
 }
 
